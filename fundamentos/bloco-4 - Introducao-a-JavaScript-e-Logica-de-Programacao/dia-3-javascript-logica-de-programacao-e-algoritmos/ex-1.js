e9649110a68ad036733ecf283caea9cbca74d517
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-11-29 19:25:50 -0300

    exercicio 1

---
+++ fundamentos/bloco-4 - Introducao-a-JavaScript-e-Logica-de-Programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex-1.js
@@ -0,0 +1,11 @@
+
+let n = 5;
+let simbolo = '*';
+let linha = '';
+
+for (let index = 0; index < n; index += 1) {
+  linha = linha + simbolo;
+}
+for (let index = 0; index < n; index += 1) {
+    console.log(linha);
+}
