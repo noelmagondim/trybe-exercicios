70e68eb3710183fc0d69754bd474dc97a50f47b9
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-01 15:09:26 -0300

    name , lastName e age - ex2

--- fundamentos/bloco-4 - Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-javascript-objetos-e-funcoes/so-para-fixar-1.js
+++ fundamentos/bloco-4 - Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-javascript-objetos-e-funcoes/so-para-fixar-1.js
@@ -1,7 +1,3 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
 let player = {
     name: 'Marta',
     lastName: 'Silva',
@@ -10,3 +6,4 @@ let player = {
         golden: 2, silver: 3 
     }
 }
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
