5d99660e9b6986070ebcf6ef9229b25fd4742702
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-01 18:38:42 -0300

    separando o exercicio 1

--- fundamentos/bloco-4 - Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
+++ fundamentos/bloco-4 - Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
@@ -2,19 +2,7 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
 };
-//console.log("Bem-vinda, " + info.personagem);
-/*
-info.recorrente = 'Sim';
-console.log(info);
-
-for (let key in info){
-    console.log(key);
-}
-
-for (let key in info){
-    console.log(info[key]);
-}*/
+console.log("Bem-vinda, " + info.personagem);
 
 
