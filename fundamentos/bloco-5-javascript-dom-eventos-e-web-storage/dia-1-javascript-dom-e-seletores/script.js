4afbb1e9432f45f4638f69961b1e3c6164e9215c
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-03 16:12:55 -0300

    alteração final

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -5,8 +5,10 @@ document.getElementById("second-paragraph").innerText = "Este é o primeiro film
 document.getElementById("subtitle").innerText = "E a pedra filosofal";
 */
 //só para fixar 2
+/*
 let par = document.getElementsByClassName("paragrafo");
 par[0].style.fontSize = '1.5rem';
 par[0].style.color = 'green';
 let sub = document.getElementsByTagName('h4');
 sub[0].style.color = 'green';
+*/
