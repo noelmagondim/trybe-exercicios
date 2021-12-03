aeec4b8be9b4aa1ce7f8476f644a55c34cf79d74
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-03 17:19:31 -0300

    só para fixar 3

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script1.js
@@ -0,0 +1,23 @@
+const header = document.getElementById("header-container");
+header.style.backgroundColor = 'rgb(0,176,105)';
+
+const footer = document.getElementById("footer-container");
+footer.style.backgroundColor = '#023031';
+
+const emergency = document.getElementsByClassName("emergency-tasks")[0];
+emergency.style.backgroundColor = '#ffbec7';
+
+const urgente1 = document.getElementsByTagName("h3")[0];
+urgente1.style.backgroundColor = '#704488';
+
+const urgente2 = document.getElementsByTagName("h3")[1];
+urgente2.style.backgroundColor = '#704488';
+
+const noEmergency = document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergency.style.backgroundColor = 'rgb(254,254,88)';
+
+const naoUrgente1 = document.getElementsByTagName("h3")[2];
+naoUrgente1.style.backgroundColor = 'rgb(106,106,106)';
+
+const naoUrgente2 = document.getElementsByTagName("h3")[3];
+naoUrgente2.style.backgroundColor = 'rgb(106,106,106)';
