3aa5e0e8cd3c0b71b466761e4cfe1ab9f54876f2
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-03 15:56:42 -0300

    só para fixar 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,3 +1,12 @@
+//só para fixar 
+/*
 document.getElementById("page-title").innerText = "Harry Potter";
 document.getElementById("second-paragraph").innerText = "Este é o primeiro filme da saga";
 document.getElementById("subtitle").innerText = "E a pedra filosofal";
+*/
+//só para fixar 2
+let par = document.getElementsByClassName("paragrafo");
+par[0].style.fontSize = '1.5rem';
+par[0].style.color = 'green';
+let sub = document.getElementsByTagName('h4');
+sub[0].style.color = 'green';
