baa65a21650fdd2a3a90cb01eda859f71bfc8346
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2021-12-03 15:37:09 -0300

    só para fixar 1 - parte 2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Harry Potter";
+document.getElementById("second-paragraph").innerText = "Este é o primeiro filme da saga";
+document.getElementById("subtitle").innerText = "E a pedra filosofal";
