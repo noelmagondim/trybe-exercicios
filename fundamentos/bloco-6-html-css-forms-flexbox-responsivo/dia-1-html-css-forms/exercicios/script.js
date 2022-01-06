7fd37c5056d621e46cc06829686b0b2b1937ee5b
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-01-06 18:25:38 -0300

    Feito alterações

--- fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -12,3 +12,12 @@ function createStateOptions() {
     }
 }
 createStateOptions();
+/*
+  Data de início - Texto
+  Verificar o formato da data dd/mm/aaaa .
+  O dia deve ser > 0 e <= 31.
+  O mês deve ser > 0 e <= 12.
+  O ano não pode ser negativo.
+  Caso alguma das condições seja inválida no momento do envio do formulário, exibir via alert uma mensagem de erro contextualizada.
+  Campo obrigatório
+*/
