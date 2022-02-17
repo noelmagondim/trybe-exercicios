edba1c1238ee697d851403e3b8aa8b52549fce11
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-02-17 14:16:23 -0300

    Exemplos do course

---
+++ front-end/bloco-10-introducao-a-react/exemplo-02/exemplo-02/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Test from './Component.js'
+
+function App() {
+  return <Test />
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/exemplo-02/exemplo-02/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Noelma Gondim</h1>
+          <p>Estudante de desenvolvimento de software pela Trybe</p>
+      </div>
+    )
+  }
+}
+
+export default Test;
